12a994d14111cf37065174694d1d67d077d4a4a2
tugot17 <piotr635@gmail.com>
2020-05-30 17:10:19 +0200

    Michalkiewicz 3d vis updated

--- testing_functions/michalkiewicz.py
+++ testing_functions/michalkiewicz.py
@@ -24,30 +24,4 @@ class Michalkiewicz(AbstractTestingFunction):
         plt.plot(x, y)
         plt.show()
 
-    def plot_3d(self, points=None):
-        fig = plt.figure()
-        ax = fig.gca(projection='3d')
 
-        # Make data.
-        X = np.arange(0, 4, 0.1)
-        Y = np.arange(0, 4, 0.1)
-
-        XY_plate = np.transpose([np.tile(X, len(Y)), np.repeat(Y, len(X))])
-
-        print(f"{XY_plate.shape}")
-
-        Z = self.__call__(XY_plate)
-
-        Z = np.reshape(Z, (len(X),len(Y)))
-
-        X, Y = np.meshgrid(X, Y)
-
-
-        # Plot the surface.
-        surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                               linewidth=0, antialiased=False)
-
-        # Add a color bar which maps values to colors.
-        fig.colorbar(surf, shrink=0.5, aspect=5)
-
-        plt.show()

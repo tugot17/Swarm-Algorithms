e217b7e1a760e62d6138325e4969c88b96aad3be
snufkin <strzalkafilip@gmail.com>
2020-06-01 21:24:06 +0200

    SPP implementation init ...

--- swarm_algorithms/particle.py
+++ swarm_algorithms/particle.py
@@ -2,8 +2,9 @@ import numpy as np
 
 
 class Particle:
-    def __init__(self, position, velocity=np.zeros(2)):
+    def __init__(self, position, velocity=np.zeros(2), angle=0.):
         self.position = position
         self.velocity = velocity
+        self.angle = angle
         self.best_known_position = np.copy(position)
         self.best_score = 0.
--- swarm_algorithms/spp.py
+++ swarm_algorithms/spp.py
@@ -1,7 +1,29 @@
 from swarm_algorithms.abstract_swarm_algorithm import AbstractSwarmAlgorithm
+import numpy as np
+from swarm_algorithms.particle import Particle
 
 
 class SPP(AbstractSwarmAlgorithm):
+    """
+    SPP are point particles, which
+        - move with a constant speed
+        - and adopt (at each time increment) the average direction of motion of the other particles
+          in their local neighborhood
+          up to some added noise.
+    """
+    def __init__(self):
+        super().__init__(optimized_function, number_of_agents)
+
+        # Initialize particles
+        p_lo, p_hi = -50., 50.
+        v = 1.0
+
+        self.particles = [Particle(position=np.random.uniform(p_lo, p_hi, 2),
+                                   angle=np.random.uniform(0., 2*np.pi),
+                                   velocity=v
+                                   )
+                          for _ in range(self.number_of_agents)]
+
 
     def get_best_global_solution(self):
         return NotImplementedError()

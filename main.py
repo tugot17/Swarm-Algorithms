f459f023f5474a8e41fd4411b430979247d55ead
snufkin <strzalkafilip@gmail.com>
2020-06-02 14:57:24 +0200

    AAA implemented and running

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from swarm_algorithms.pso import PSO
 from swarm_algorithms.spp import SPP
+from swarm_algorithms.aaa import AAA
 from testing_functions.auckley import Auckley
 from testing_functions.michalkiewicz import Michalkiewicz
 from tqdm import trange
 import numpy as np
 
-NUMBER_OF_STEPS = 2000
+NUMBER_OF_STEPS = 30
 
-NUMBER_OF_AGENTS = 25
+NUMBER_OF_AGENTS = 50
 
 OPTIMIZED_FUNCTON = Auckley()
 #OPTIMIZED_FUNCTON = Michalkiewicz()
 
 if __name__ == '__main__':
-    swarm = PSO(optimized_function=OPTIMIZED_FUNCTON, number_of_agents=NUMBER_OF_AGENTS)
+    swarm = AAA(optimized_function=OPTIMIZED_FUNCTON, number_of_agents=NUMBER_OF_AGENTS)
 
     particles_in_step = []
 
     for i in trange(NUMBER_OF_STEPS):
         swarm.step()
-        if i % 100 == 0:
+        if i % 3 == 0:
             print(swarm.optimized_function(swarm.best_solution), swarm.best_solution)
             OPTIMIZED_FUNCTON.plot_2d(points=np.array([particle.position for particle in swarm.particles]), dirs=np.array([particle.velocity for particle in swarm.particles]))
 
--- swarm_algorithms/aaa.py
+++ swarm_algorithms/aaa.py
@@ -1,10 +1,64 @@
 from swarm_algorithms.abstract_swarm_algorithm import AbstractSwarmAlgorithm
+import numpy as np
+
+from swarm_algorithms.particle import Particle
 
 
 class AAA(AbstractSwarmAlgorithm):
+    # Hyperparameters
+    w_min, w_max = np.pi / 2, 3 * np.pi / 2
+
+    a_min, a_max = 0., 2 * np.pi
+    p_lo, p_hi = -50., 50.
+
+    survival_coeff = 0.7
+    s_sigma = 0.01
+
+
+    def __init__(self, optimized_function, number_of_agents):
+        super().__init__(optimized_function, number_of_agents)
+
+        # Initialize particles
+        self.particles = [Particle(position=np.random.uniform(self.p_lo, self.p_hi, 2))
+                          for _ in range(self.number_of_agents)]
+
+        # Update the swarm best known position
+        self.best_swarm_score = np.inf
+        for particle in self.particles:
+            particle.best_score = self.optimized_function(particle.position)
+            if particle.best_score < self.best_swarm_score:
+                self.best_swarm_score = particle.best_score
+                self.best_solution = particle.best_known_position
 
     def get_best_global_solution(self):
-        return NotImplementedError()
+        return self.best_solution
 
     def step(self):
-        return NotImplementedError()
+        r_max = np.mean([np.sqrt((p1.position - p2.position) ** 2) for p1 in self.particles for p2 in self.particles])/2.
+
+        w = np.random.uniform(self.w_min, self.w_max)
+        for particle in self.particles:
+            a = np.random.uniform(self.a_min, self.a_max)
+            r = np.random.uniform(0., r_max)
+
+            v1 = np.array([np.cos(a), np.sin(a)]) * r
+            v2 = np.array([np.cos(w + a - np.pi), np.sin(w + a - np.pi)]) * r
+
+            particle.position = particle.position + v1 + v2
+
+        #starvation
+        energy = [(particle, -self.optimized_function(particle.position)) for particle in self.particles]
+        energy = list(sorted(energy, key=lambda x: -x[1]))
+        survived = [p for p, _ in energy[:int(self.survival_coeff * len(energy))]]
+
+        self.particles = survived + [Particle(survived[i].position + np.random.normal(0., self.s_sigma, survived[i].position.shape))
+                                     for i in range(len(self.particles) - len(survived))]
+
+        for particle in self.particles:
+            particle_new_score = self.optimized_function(particle.position)
+            if particle_new_score <= particle.best_score:
+                particle.best_score = particle_new_score
+                particle.best_known_position = np.copy(particle.position)
+                if particle.best_score <= self.best_swarm_score:
+                    self.best_swarm_score = particle.best_score
+                    self.best_solution = particle.best_known_position

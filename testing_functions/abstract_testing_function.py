8f0153dbb3960d3b7edd80e4fa20617f71aadd48
tugot17 <piotr635@gmail.com>
2020-06-02 12:22:44 +0200

    Plotting updated

--- testing_functions/abstract_testing_function.py
+++ testing_functions/abstract_testing_function.py
@@ -17,22 +17,25 @@ class AbstractTestingFunction(ABC):
         # Make data.
         dx_max = np.max(points[:, 0])
         dy_max = np.max(points[:, 1])
+
         dx_min = np.min(points[:, 0])
         dy_min = np.min(points[:, 1])
 
-        eps = 0.1
-
         d_min = min(dx_min, dy_min)
-        d_max = min(dx_max, dy_max)
+        d_max = max(dx_max, dy_max)
 
-        X = np.arange(d_min, max(d_max, d_min+eps), max(d_max - d_min, eps) / 50.)
-        Y = np.arange(d_min, max(d_max, d_min+eps), max(d_max - d_min, eps) / 50.)
+        d_min = min(-1, d_min)
+        d_max = max(1, d_max)
 
-        # Y = np.arange(dy_min, max(dy_max, dy_min+eps), max(dy_max - dy_min, eps) / 50.)
+
+        X = np.arange(d_min, d_max + (d_max - d_min) / 50., (d_max - d_min) / 50.)
+        Y = np.arange(d_min, d_max + (d_max - d_min) / 50., (d_max - d_min) / 50.)
+
         XY_plate = np.transpose([np.tile(X, len(Y)), np.repeat(Y, len(X))])
 
         Z = self.__call__(XY_plate)
         Z = np.reshape(Z, (len(X), len(Y)))
+
 
         # Plot the surface.
         pl = plt.contourf(X, Y, Z)
@@ -42,7 +45,6 @@ class AbstractTestingFunction(ABC):
         plt.scatter(points[:, 0], points[:, 1], c='r')
 
         # Plot the dirs
-        print(points.shape, dirs.shape)
         plt.quiver(points[:, 0], points[:, 1], dirs[:, 0], dirs[:, 1])
 
         plt.show()
